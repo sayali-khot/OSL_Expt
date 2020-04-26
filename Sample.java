0ae2d0bed898102df48786026c4213c0c37a2dd5
sayali-khot <sayali.khot@somaiya.edu>
2020-04-26 16:24:27 +0530

    Added new files

---
+++ Sample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sample{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+			
+				System.out.println("Sample file 1");
+	}
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Test{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+			
+				System.out.println("Test file 1");
+	}
+}
