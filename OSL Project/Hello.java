83cca1d99b4c315b7459df0abca2e2f0cfc516cc
sayali-khot <54390920+sayali-khot@users.noreply.github.com>
2020-04-26 15:13:14 +0530

    Add files via upload
    uploading a folder

---
+++ OSL Project/Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Hello{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+			
+				System.out.println("Hello World");
+	}
+}
